cbee27b5347507fa62adc81b50e836f2460022ab
Mahfujur Rahman Sifat <sifat.code@gmail.com>
2023-02-06 20:27:37 +0600

    Scanf programm

--- main.go
+++ main.go
@@ -3,7 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	var name string
+	var age int
 
-	fmt.Println("Hellow sifat")
-	fmt.Println("Test Git-GitHub")
+	fmt.Println("Enter your name =")
+	fmt.Scanf("%s", &name)
+	fmt.Println("Enter your age =")
+	fmt.Scanf("%d", &age)
+
+	fmt.Printf("Your name is %s and age %d", name, age)
+
 }
