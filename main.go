d65aefcaaaafe3bbd3c7d907035b36e69528d015
Mahfujur Rahman Sifat <sifat.code@gmail.com>
2023-02-07 23:42:01 +0600

    Array

--- main.go
+++ main.go
@@ -4,17 +4,10 @@ import "fmt"
 
 func main() {
 
-	BookInfo := struct {
-		title   string
-		authore string
-		ISBN    int32
-		price   float32
-	}{
-		title:   "The GO",
-		authore: "Sifat",
-		ISBN:    6895418,
-		price:   125,
-	}
-	fmt.Println(BookInfo)
+	var array [2]string
+	array[0] = "Hello"
+	array[1] = "SIFAT"
+
+	fmt.Println(array)
 
 }
