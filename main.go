78ce1fdc59aa0c979163bee56c4afafeeae795b6
Mahfujur Rahman Sifat <sifat.code@gmail.com>
2023-02-08 23:53:33 +0600

    string

--- main.go
+++ main.go
@@ -4,7 +4,11 @@ import "fmt"
 
 func main() {
 	var student [3]string
+	student[0] = "sifat"
+	student[1] = "muhammad"
+	student[2] = "mahfuj"
+
 	fmt.Println(student)
-	fmt.Println(len(student))
+	fmt.Println("string length =", len(student))
 
 }
