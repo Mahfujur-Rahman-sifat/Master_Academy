9ccbe2d39a9e66695ddc431567968b12d31afde4
Mahfujur Rahman Sifat <sifat.code@gmail.com>
2023-02-08 23:47:49 +0600

    string len

--- main.go
+++ main.go
@@ -3,27 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var day int
-	fmt.Println("Enter number of day")
-	fmt.Scanf("%d", &day)
-
-	switch day {
-	case 1:
-		fmt.Println("Monday")
-	case 2:
-		fmt.Println("Tuesday")
-	case 3:
-		fmt.Println("Wednesday")
-	case 4:
-		fmt.Println("Thursday")
-	case 5:
-		fmt.Println("Friday")
-	case 6:
-		fmt.Println("Saturday")
-	case 7:
-		fmt.Println("Sunday")
-	default:
-		fmt.Println("Invalid")
-	}
+	var student [3]string
+	fmt.Println(student)
+	fmt.Println(len(student))
 
 }
