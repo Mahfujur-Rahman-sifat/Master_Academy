8a284102e61e5d6f550e44b974c383288b209a53
Mahfujur Rahman Sifat <sifat.code@gmail.com>
2023-02-08 23:35:33 +0600

    switch

--- main.go
+++ main.go
@@ -3,11 +3,27 @@ package main
 import "fmt"
 
 func main() {
+	var day int
+	fmt.Println("Enter number of day")
+	fmt.Scanf("%d", &day)
 
-	var array [2]string
-	array[0] = "Hello"
-	array[1] = "SIFAT"
-
-	fmt.Println(array)
+	switch day {
+	case 1:
+		fmt.Println("Monday")
+	case 2:
+		fmt.Println("Tuesday")
+	case 3:
+		fmt.Println("Wednesday")
+	case 4:
+		fmt.Println("Thursday")
+	case 5:
+		fmt.Println("Friday")
+	case 6:
+		fmt.Println("Saturday")
+	case 7:
+		fmt.Println("Sunday")
+	default:
+		fmt.Println("Invalid")
+	}
 
 }
