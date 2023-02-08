559c855d8b25c486dfef7d6cc0076d14b15dd7bd
Mahfujur Rahman Sifat <sifat.code@gmail.com>
2023-02-08 23:59:26 +0600

    slice

--- main.go
+++ main.go
@@ -3,12 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	var student [3]string
-	student[0] = "sifat"
-	student[1] = "muhammad"
-	student[2] = "mahfuj"
 
-	fmt.Println(student)
-	fmt.Println("string length =", len(student))
+	// Creating an array
+	arr := [7]string{"This", "is", "the", "tutorial",
+		"of", "Go", "language"}
 
+	// Display array
+	fmt.Println("Array:", arr)
+
+	// Creating a slice
+	myslice := arr[1:6]
+
+	// Display slice
+	fmt.Println("Slice:", myslice)
+
+	// Display length of the slice
+	fmt.Printf("Length of the slice: %d", len(myslice))
+
+	// Display the capacity of the slice
+	fmt.Printf("\nCapacity of the slice: %d", cap(myslice))
 }
