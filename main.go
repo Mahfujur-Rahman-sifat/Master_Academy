2f2ab651da97b6bccc0997b3e5847361767abb67
Mahfujur Rahman Sifat <sifat.code@gmail.com>
2023-02-15 10:40:44 +0600

    if condition

--- main.go
+++ main.go
@@ -4,22 +4,13 @@ import "fmt"
 
 func main() {
 
-	// Creating an array
-	arr := [7]string{"This", "is", "the", "tutorial",
-		"of", "Go", "language"}
+	fmt.Println("Enter your age")
+	var age int
+	fmt.Scanf("%d", &age)
+	if age < 18 {
+		fmt.Println("Children")
+	} else if age >= 18 {
+		fmt.Println("Adult")
 
-	// Display array
-	fmt.Println("Array:", arr)
-
-	// Creating a slice
-	myslice := arr[1:6]
-
-	// Display slice
-	fmt.Println("Slice:", myslice)
-
-	// Display length of the slice
-	fmt.Printf("Length of the slice: %d", len(myslice))
-
-	// Display the capacity of the slice
-	fmt.Printf("\nCapacity of the slice: %d", cap(myslice))
+	}
 }
